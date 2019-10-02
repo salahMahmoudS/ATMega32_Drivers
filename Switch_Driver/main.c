65d1e6bab95793ff018eaf45b65fce1ac8cd65f9
Salah Mahmoud <SalahMahmoud202@gmail.com>
2019-10-02 19:56:55 +0300

    added proteus example

--- Switch_Driver/main.c
+++ Switch_Driver/main.c
@@ -1,4 +1,4 @@
-#include "std_types.h"
+	#include "std_types.h"
 #include "Bit_Math.h"
 #include "DIO_Int.h"
 #include "Switch_Init.h"
@@ -6,13 +6,13 @@
 int main(void){
 	DIO_voidInitialize();
 	Switch_voidInitialize();
-DIO_voidWriteOnPin(DIO_PIN_1,DIO_HIGH);
+DIO_voidWriteOnPin(DIO_PIN_6,DIO_HIGH); //Activate pullup resistor in pin6 the microcontroller that is connected to switch
 	while (1){
 		if (Switch_u8IsPressed(SWITCH_0) == IS_PRESSED){
-			DIO_voidWriteOnPin(DIO_PIN_1,DIO_HIGH);
+			DIO_voidWriteOnPin(DIO_PIN_1,DIO_HIGH); //light led if switch is pressed
 		}
 		else {
-			DIO_voidWriteOnPin(DIO_PIN_1,DIO_LOW);
+			DIO_voidWriteOnPin(DIO_PIN_1,DIO_LOW); //turn off led
 		}
 	}
 	return 0;
